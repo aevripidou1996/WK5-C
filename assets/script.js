e82937a744734549eb7bf1e0aecd26905add4942
Andrew Evripidou <aevripidou1996@gmail.com>
2023-03-13 19:56:23 +1100

    added-time-var

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
-  
+var currentTimeDisplay = $('#currentDay');
+currentTimeDisplay.text(moment().format('1111'));
+
+var hourNine = $('#hour-9');
+var hourTen = $("#hour-10");
+var hourEleven = $("#hour-11");
+var hourTwelve = $("#hour-12");
+var hourThirteen = $("#hour-13");
+var hourFourteen = $("#hour-14");
+var hourFithteen = $("#hour-15");
+var hourSixteen = $('#hour-16');
+var hourSeventeen = $('#hour-17');
 
 
+var saveBtn = $(".saveBtn");
+
+
+saveBtn.on("click", function(e) {
+    e.preventDefault();
+    hourNineVal = hourNine.val();
+    hourTenVal = hourTen.val();
+    hourElevenVal = hourEleven.val();
+    hourTwelveVal =  hourTwelve.val();
+    hourThirteenVal = hourThirteen.val(); 
+    hourFourteenVal = hourFourteen.val(); 
+    hourFifteenVal = hourFifteen.val();
+    hourSixteenVal = hourSixteen.val();
+    hourSeventeenVal = hourSeventeen.val();
+    localStorage.setItem("hour09", hourNineVal);
+    localStorage.setItem("hour10", hourTenVal);
+    localStorage.setItem("hour11", hourElevenVal);
+    localStorage.setItem("hour12", hourTwelveVal);
+    localStorage.setItem("hour13", hourThirteenVal);
+    localStorage.setItem("hour14", hourFourteenVal);
+    localStorage.setItem("hour15", hourFifteenVal);
+    localStorage.setItem("hour16", hourSixteenVal);
+    localStorage.setItem("hour17", hourSeventeenVal);
+});
